6ebced424e701bf39caa7dc6364a63f26c1882cf
Eduardo Wada <156962925+eduardowada@users.noreply.github.com>
2024-08-14 17:03:15 -0300

    Update PostagemController.java

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -17,6 +17,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/postagens")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PostagemController {
 
     @Autowired
